8a1cff7a079e88cd559a78d2256b9c76ff5bb5ba
harsh makadiya <harsh.makadiya84@gmail.com>
2024-01-21 18:12:10 +0530

    http client

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
+	"text/template"
 )
 
 func Handle(w http.ResponseWriter, r *http.Request) {
@@ -23,6 +26,31 @@ func filee(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() //Parse url parameters passed, then parse the response packet for the POST body (request body)
+	// attention: If you do not call ParseForm method, the following data can not be obtained form
+	fmt.Println(r.Form) // print information on server side.
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello astaxie!") // write data to response
+}
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //get request method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		// logic part of log in
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
+}
 func main() {
 	// fileServer := http.FileServer(http.Dir("./static"))
 	// http.Handle("/", fileServer)
@@ -31,6 +59,15 @@ func main() {
 	// if err := http.ListenAndServe(":8080", nil); err != nil {
 	// 	log.Fatal(err)
 	// }
+
+	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("listenAndServe:", err)
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////
 	// var gram1, rate1 float64
 	const fix = 1000
 	// fmt.Print("enter gram you want")
